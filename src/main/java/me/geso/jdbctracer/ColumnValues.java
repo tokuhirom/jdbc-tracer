e2ca7201af7c0855cc4512505af40a5508520658
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-04-03 23:49:04 +0900

    Added test case for ColumnValues class

--- src/main/java/me/geso/jdbctracer/ColumnValues.java
+++ src/main/java/me/geso/jdbctracer/ColumnValues.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @ToString
-public class ColumnValues {
+class ColumnValues {
     private Map<Integer, Object> columnValues = new HashMap<>();
 
     void put(Integer key, Object value) {
